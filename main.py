ec06c27ccaa4bce267d8824a5a26f93586a576b1
Gustavo <gustavohenriqueferreirasantosf@gmail.com>
2024-09-30 11:27:26 -0300

    modified:

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import tkinter as tk # usa para criar interfaces graficas, como: janela principa
 from tkinter import ttk, font, messagebox, BOTH
 from tkinter import PhotoImage
 
+
 # criando janela
 window = tk.Tk()
 window.title("Meu app de tarefas")
@@ -11,6 +12,7 @@ window.configure(background="#F0F0F0")
 window.geometry("500x600")
 
 frame_in_edition = None
+tasks = []
 
 # função para adicionar tarefa
 def add_task():
@@ -29,7 +31,13 @@ def add_task():
 
 
 def add_item_task(task):
+    global tasks
+
     frame_task = tk.Frame(internal_canvas, background="white", bd=1, relief=tk.SOLID)
+
+    check_var = tk.BooleanVar()
+    task_data = {"frame" : frame_task, "task" : task, "status" : "Pendente", "check_var": check_var}
+    tasks.append(task_data)
 
     label_task = tk.Label(frame_task, text=task, font=("Garamond", 16), background="white", width=25, height=2, anchor="w")
     label_task.pack(side=tk.LEFT, fill=tk.X, padx=10, pady=5)
@@ -43,7 +51,7 @@ def add_item_task(task):
     # para posicionar o frame task
     frame_task.pack(fill=tk.X, padx=5, pady=5)
 
-    check_button = ttk.Checkbutton(frame_task, command=lambda label=label_task: change_underline(label))
+    check_button = ttk.Checkbutton(frame_task, variable=task_data["check_var"], command=lambda : change_status(task_data))
     check_button.pack(side=tk.RIGHT, padx=5)
 
     internal_canvas.update_idletasks()
@@ -70,25 +78,48 @@ def delete_task(frame_task):
     internal_canvas.update_idletasks()
     canvas.config(scrollregion=canvas.bbox("all"))
 
-
-def change_underline(label):
-    font_present = label.cget("font")
-    if "overstrike" in font_present:
-        new_font = font_present.replace(" overstrike", "")
+# mudar o status da tarefa
+def change_status(task_data):
+    if task_data["check_var"].get():
+        task_data["status"] = "Concluida"
     else:
-        new_font = font_present + " overstrike" 
-    label.config(font=new_font)
+        task_data["status"] = "Pendente"
+    apply_filter() # replicar o filtro sempre que o status mudar 
+    
+
+# função para aplicar o filtro
+def apply_filter(status_filter = "Todos"):
+    for task_data in tasks:
+        if status_filter == "Todos":
+            # fill = tk.X diz para expandir horizontalmente para preencher todo o espaço disponivel
+            task_data["frame"].pack(fill=tk.X, padx=5, pady=5)
+        elif status_filter == "Concluidas" and task_data["status"] == "Concluida":
+            task_data["frame"].pack(fill=tk.X, padx=5, pady=5)
+        elif status_filter== "Pendentes" and task_data["status"] == "Pendente":
+            task_data["frame"].pack(fill=tk.X, padx=5, pady=5)
+        else:
+            task_data["frame"].pack_forget() # esconde a tarefa
+
+# funções para os botões
+def show_all_tasks():
+    apply_filter("Todos")
+
+
+def show_complete_tasks():
+    apply_filter("Concluidas")
+
+
+def show_pending_tasks():
+    apply_filter("Pendentes")
 
 
 edit_icon = PhotoImage(file="edit.png").subsample(20, 20)
 delete_icon = PhotoImage(file="delete.png").subsample(20, 20)
 
-
 # fonte do cabeçalho
 header_font = font.Font(family="Garamond", size=24, weight="bold")
 # criando rótulo
 header_label = tk.Label(window, text="Meu App de Tarefas", font = header_font, background="#F0F0F0", fg="#333").pack(pady=20)
-
 
 frame = tk.Frame(window, background="#F0F0F0")
 frame.pack(pady=10)
@@ -112,6 +143,19 @@ canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 # definindo o scroll bar
 scroll_bar = ttk.Scrollbar(task_list_frame, orient="vertical", command=canvas.yview)
 scroll_bar.pack(side=tk.RIGHT, fill=tk.Y)
+
+# adicionar os botões
+filter_name = tk.Frame(window, background="#F0F0F0")
+filter_name.pack(pady=10)
+
+button_all = tk.Button(filter_name, text="Todas", command=show_all_tasks, background="#4CAF50", fg="white", width=10)
+button_all.pack(side=tk.LEFT, padx=5)
+
+button_complete = tk.Button(filter_name, text="Concluídas", command=show_complete_tasks, background="#4CAF50", fg="white", width=10)
+button_complete.pack(side=tk.LEFT, padx=5)
+
+button_peding = tk.Button(filter_name, text="Pendentes", command=show_pending_tasks, background="#4CAF50", fg="white", width=10)
+button_peding.pack(side=tk.LEFT, padx=5)
 
 # para que o scrool bar possa aparecer
 canvas.configure(yscrollcommand=scroll_bar.set)
